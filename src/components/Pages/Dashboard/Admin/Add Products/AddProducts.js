6c6c82725703bac225442c7f11aac49a7b682050
rahatkabir007 <rahatkabir04@gmail.com>
2021-11-12 10:27:37 +0600

    add products done

--- src/components/Pages/Dashboard/Admin/Add Products/AddProducts.js
+++ src/components/Pages/Dashboard/Admin/Add Products/AddProducts.js
@@ -1,10 +1,69 @@
 import React from 'react';
+import axios from 'axios';
+import { useForm } from "react-hook-form";
+import useAuth from '../../../../hooks/useAuth';
+import './AddProducts.css';
 
 const AddProducts = () => {
+    const { user } = useAuth();
+    const { register, handleSubmit, formState: { errors } ,reset} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+        axios.post('http://localhost:5000/products', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    alert("Successfully Added");
+                    reset();
+                }
+            })
+    }
     return (
-        <div>
-            <h2>This is add products</h2>
-        </div>
+        <>
+            <div className="container my-4">
+                <form onSubmit={handleSubmit(onSubmit)} className="w-50 mx-auto text-center add-form-area">
+                    <input
+                        {...register("name")}
+                        defaultValue={user?.displayName}
+                        className="p-2 m-2 w-100"
+                    />
+                    <input
+                        {...register("email")}
+                        defaultValue={user?.email}
+                        className="p-2 m-2 w-100"
+                    />
+                    <input
+                        {...register("car-name", { required: true })}
+                        placeholder="Name"
+                        className="p-2 m-2 w-100"
+                    />
+                    <textarea
+                        {...register("description", { required: true })}
+                        placeholder="Description"
+                        className="p-2 m-2 w-100"
+                    />
+                    <input
+                        {...register("price", { required: true })}
+                        placeholder="Price"
+                        className="p-2 m-2 w-100"
+                    />
+                    <input
+                        {...register("image", { required: true })}
+                        placeholder="Image Link"
+                        className="p-2 m-2 w-100"
+                    />
+
+                    <br />
+                    {errors.exampleRequired && <span>This field is required</span>}
+
+                    <div className="">
+                        <input type="submit" value="ADD" className="primary-btn px-5 my-2" />
+                    </div>
+                </form>
+            </div>
+
+
+        </>
     );
 };
 
