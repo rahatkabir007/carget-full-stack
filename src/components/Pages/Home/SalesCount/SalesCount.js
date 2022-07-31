a0d6127a9ce26b82c5398cfcfabd13bde50d053a
rahatkabir007 <rahatkabir04@gmail.com>
2022-07-31 22:09:01 +0600

    count change

--- src/components/Pages/Home/SalesCount/SalesCount.js
+++ src/components/Pages/Home/SalesCount/SalesCount.js
@@ -1,54 +1,55 @@
 import { Grid, Container } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
-import AnimatedNumber from 'react-animated-number';
+
+import CountUp from 'react-countup';
 
 const SalesCount = () => {
+
+    const counts = [
+        {
+            value: 5682,
+            title: 'Brand New Cars'
+        },
+        {
+            value: 2572,
+            title: 'Certified Pre-Owned Vehicles'
+        },
+        {
+            value: 9562,
+            title: 'Pre-Owned Vehicles'
+        },
+    ]
+
     return (
-      
-        <Box sx={{ backgroundColor: '#f18d4d', py: 10}}>
+
+        <Box sx={{ backgroundColor: '#f18d4d', py: 10 }}>
             <Container>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} md={12} lg={4} sx={{ textAlign: 'center', color: 'black', textShadow: '1px 1px grey',  }}>
-                        <AnimatedNumber component="text" value={5682}
-                            style={{
-                                transition: '0.6s ease-out',
-                                fontSize: 80,
-                                letterSpacing: '10px',
-                            }}
-                            formatValue={n => n.toFixed()}
-                            duration={3000}
-                        />
-                        <h3>Brand New Cars</h3>
-                    </Grid>
-                    <Grid item xs={12} md={12} lg={4} sx={{ textAlign: 'center', color: 'white', textShadow: '1px 1px grey' }}>
-                        <AnimatedNumber component="text" value={2572}
-                            style={{
-                                transition: '0.6s ease-out',
-                                fontSize: 80,
-                                letterSpacing: '10px'
-                            }}
-                            formatValue={n => n.toFixed()}
-                            duration={3000}
-                        />
-                        <h3>Certified Pre-Owned Vehicles</h3>
-                    </Grid>
-                    <Grid item xs={12} md={12} lg={4} sx={{ textAlign: 'center', color: 'black', textShadow: '1px 1px grey'  }}>
-                        <AnimatedNumber component="text" value={9562}
-                            style={{
-                                transition: '0.6s ease-out',
-                                fontSize: 80,
-                                letterSpacing: '10px'
-                            }}
+                    {
+                        counts.map((count, index) => {
+                            return (
+                                <Grid item xs={12} md={12} lg={4} sx={{ textAlign: 'center', color: 'black', textShadow: '1px 1px grey', }}>
 
-                            formatValue={n => n.toFixed()}
-                            duration={3000}
-                        />
-                        <h3>Pre-Owned Vehicles</h3>
-                    </Grid>
+                                    <CountUp
+                                        delay={0}
+                                        duration={1}
+
+                                        end={count.value}
+                                        style={{
+                                            transition: '0.6s ease-out',
+                                            fontSize: 80,
+                                            letterSpacing: '10px',
+                                        }}
+                                    />
+                                    <h3>{count.title}</h3>
+                                </Grid>
+                            )
+                        })
+                    }
                 </Grid>
-          </Container>
-              
+            </Container>
+
         </Box>
     );
 };
